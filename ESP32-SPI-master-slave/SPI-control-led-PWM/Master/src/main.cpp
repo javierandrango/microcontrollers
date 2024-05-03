f96cbf9073256eaeefda8b36a9266e3a706eeaaa
javierandrango <lab_iarobotico593@outlook.com>
2024-05-03 00:34:26 -0500

    ESP32SPI, led-PWM: send led status from Slave to Master

--- ESP32-SPI-master-slave/SPI-control-led-PWM/Master/src/main.cpp
+++ ESP32-SPI-master-slave/SPI-control-led-PWM/Master/src/main.cpp
@@ -21,6 +21,7 @@ uint8_t rx_buf[BUFFER_SIZE]{0,0,0,0};
 uint8_t potVal = 0;
 
 /*FUNCTION DECLARATION------------------------------------------------------------------------------------------------------*/
+
 /*--------------------------------------------------------------------------------------------------------------------------*/
 
 /*VOID SETUP CONFIGURATION--------------------------------------------------------------------------------------------------*/
@@ -29,6 +30,7 @@ void setup() {
   delay(2000);
   // Initialize SPI bus with defined pins as MASTER
   pinMode(SPI_SS,OUTPUT);
+  pinMode(SPI_MOSI,OUTPUT);
   digitalWrite(SPI_SS,HIGH);
   master.begin(SPI_SCK,SPI_MISO,SPI_MOSI,SPI_SS);
   // Wait for SPI to stabilize
@@ -39,22 +41,50 @@ void setup() {
 
 /*LOOP----------------------------------------------------------------------------------------------------------------------*/
 void loop(){
-  
+
   // Read Potentiometer values
   potVal = map(analogRead(potPin),0,4095,0,255);
   // Add potentiometer value to buffer
   tx_buf[0] = potVal;
-  
-  // Sends an encoded message 
+
+  // Sends potentiometer value to Slave
   master.beginTransaction(SPISettings(clock_speed, MSBFIRST, SPI_MODE0));
   digitalWrite(SPI_SS, LOW);
-  master.transfer(tx_buf, BUFFER_SIZE);
-  //master.transferBytes(tx_buf, NULL, BUFFER_SIZE);
+  master.transferBytes(tx_buf, NULL, BUFFER_SIZE);
   digitalWrite(SPI_SS, HIGH);
   master.endTransaction();
 
+  // Receives Led state from Master 
+  master.beginTransaction(SPISettings(clock_speed, MSBFIRST, SPI_MODE0));
+  digitalWrite(SPI_SS, LOW);
+  master.transferBytes(NULL, rx_buf, BUFFER_SIZE);
+  digitalWrite(SPI_SS, HIGH);
+  master.endTransaction();
+
+
   Serial.print("pot value: ");
   Serial.println(potVal);
+
+  // Shows response from Slave
+    Serial.print("Led status: ");
+    switch (rx_buf[0]){
+    case 1:
+      // led off
+      Serial.println("OFF");
+      break;
+    case 2:
+      // led fading
+      Serial.println("FADING");
+      break;
+    case 3:
+      // led on
+      Serial.println("ON");
+      break;
+    default:
+      // no response
+      Serial.println("---");
+      break;
+    }
 }
 /*--------------------------------------------------------------------------------------------------------------------------*/
 
--- ESP32-SPI-master-slave/SPI-control-led-PWM/Slave/src/main.cpp
+++ ESP32-SPI-master-slave/SPI-control-led-PWM/Slave/src/main.cpp
@@ -9,7 +9,7 @@
 // SPI settings
 ESP32SPISlave slave;
 // Queued transactions
-static constexpr size_t QUEUE_SIZE = 1;
+static constexpr size_t QUEUE_SIZE = 2;
 // Buffer size must be multiples of 4 bytes
 static constexpr size_t BUFFER_SIZE = 4;
 uint8_t tx_buf[BUFFER_SIZE]{0,0,0,0};
@@ -22,6 +22,7 @@ uint8_t led_resolution = 8;                                // 8-bit resolution (
 uint32_t led_freq = 1200;                                  // PWM frecuency    
 
 /*FUNCTION DECLARATION------------------------------------------------------------------------------------------------------*/
+
 /*--------------------------------------------------------------------------------------------------------------------------*/
 
 /*VOID SETUP CONFIGURATION--------------------------------------------------------------------------------------------------*/
@@ -35,12 +36,11 @@ void setup() {
   
   delay(2000);
   //Initialize SPI bus with defined pins as SLAVE
+  pinMode(SPI_MISO,OUTPUT);
   slave.setDataMode(SPI_MODE0); 
   slave.setQueueSize(QUEUE_SIZE);
   // VSPI or HSPI (virtual or hardward SPI)
   slave.begin(VSPI,SPI_SCK,SPI_MISO,SPI_MOSI,SPI_SS); 
-  delay(2000);
-  
   Serial.println("start spi slave");
 }
 /*--------------------------------------------------------------------------------------------------------------------------*/
@@ -50,20 +50,43 @@ void loop(){
 
   // if no transaction is in flight and all results are handled, queue new transactions
   if (slave.hasTransactionsCompletedAndAllResultsHandled()) {
-    // Receives an encoded message 
+    // Receives potentiometer values from Slave
     slave.queue(NULL, rx_buf, BUFFER_SIZE);
+    // Sends Led status to Master
+    delay(5);
+    slave.queue(tx_buf, NULL, BUFFER_SIZE);
     // Trigger transaction in the background
     slave.trigger();
   }
-  
+
   if (slave.hasTransactionsCompletedAndAllResultsReady(QUEUE_SIZE)) {
     // Get received bytes for all transactions
-    //const std::vector<size_t> received_bytes = slave.numBytesReceivedAll();
-    size_t received_bytes = slave.numBytesReceived(); 
+    const std::vector<size_t> received_bytes = slave.numBytesReceivedAll();
+    //size_t received_bytes = slave.numBytesReceived(); 
     
     // Message received from master
     ledcWrite(1,rx_buf[0]);                                // Control PWM on channel 1
-    Serial.println(rx_buf[0]);
+    // Shows the buffer
+    for (int i=0; i<received_bytes[0];i++){
+      Serial.print(rx_buf[i]);
+      Serial.print('|');
+    }
+    Serial.println();
+    // Attach response
+    switch (rx_buf[0]){
+    case 0:
+      // led off
+      tx_buf[0] = 1;
+      break;
+    case 255:
+      // led on
+      tx_buf[0] = 3;
+      break;
+    default:
+      // led fading
+      tx_buf[0] = 2;
+      break;
+    }
   }
 }
 /*--------------------------------------------------------------------------------------------------------------------------*/
