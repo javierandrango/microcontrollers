7fe497b8838f06f399ae0ba713cbe760c62f5c2c
javierandrango <lab_iarobotico593@outlook.com>
2024-06-05 18:59:14 -0500

    ESP32-CAM, video-streaming: stream jpeg images with asyn web server

---
+++ ESP32-CAM/video-streaming/include/AsyncWebCamera.h
@@ -0,0 +1,176 @@
+#include <CameraPins.h>
+#include <esp_camera.h>
+#include <ESPAsyncWebServer.h>
+
+
+typedef struct {
+    camera_fb_t * fb;
+    size_t index;
+} camera_frame_t;
+
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";    
+static const char * JPG_CONTENT_TYPE = "image/jpeg";
+
+// init camera
+void Camera_init_cofig(){
+    camera_config_t config;
+    // camera flash led
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    // camera hardware configuration
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    // configuration for streaming
+    config.frame_size = FRAMESIZE_UXGA;
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+
+    // initialize camera
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
+    else{
+        Serial.printf("Camera init successfully!\n");
+    }
+}
+
+//Stream jpg 
+class AsyncJpegStreamResponse: public AsyncAbstractResponse {
+    private:
+        camera_frame_t _frame;
+        size_t _index;
+        size_t _jpg_buf_len;
+        uint8_t * _jpg_buf;
+        uint64_t lastAsyncRequest;
+    public:
+        AsyncJpegStreamResponse(){
+            _callback = nullptr;
+            _code = 200;
+            _contentLength = 0;
+            _contentType = STREAM_CONTENT_TYPE;
+            _sendContentLength = false;
+            _chunked = true;
+            _index = 0;
+            _jpg_buf_len = 0;
+            _jpg_buf = NULL;
+            lastAsyncRequest = 0;
+            memset(&_frame, 0, sizeof(camera_frame_t));
+        }
+        ~AsyncJpegStreamResponse(){
+            if(_frame.fb){
+                if(_frame.fb->format != PIXFORMAT_JPEG){
+                    free(_jpg_buf);
+                }
+                esp_camera_fb_return(_frame.fb);
+            }
+        }
+        bool _sourceValid() const {
+            return true;
+        }
+        virtual size_t _fillBuffer(uint8_t *buf, size_t maxLen) override {
+            size_t ret = _content(buf, maxLen, _index);
+            if(ret != RESPONSE_TRY_AGAIN){
+                _index += ret;
+            }
+            return ret;
+        }
+        size_t _content(uint8_t *buffer, size_t maxLen, size_t index){
+            if(!_frame.fb || _frame.index == _jpg_buf_len){
+                if(index && _frame.fb){
+                    uint64_t end = (uint64_t)micros();
+                    int fp = (end - lastAsyncRequest) / 1000;
+                    log_printf("Size: %uKB, Time: %ums (%.1ffps)\n", _jpg_buf_len/1024, fp);
+                    lastAsyncRequest = end;
+                    if(_frame.fb->format != PIXFORMAT_JPEG){
+                        free(_jpg_buf);
+                    }
+                    esp_camera_fb_return(_frame.fb);
+                    _frame.fb = NULL;
+                    _jpg_buf_len = 0;
+                    _jpg_buf = NULL;
+                }
+                if(maxLen < (strlen(STREAM_BOUNDARY) + strlen(STREAM_PART) + strlen(JPG_CONTENT_TYPE) + 8)){
+                    //log_w("Not enough space for headers");
+                    return RESPONSE_TRY_AGAIN;
+                }
+                //get frame
+                _frame.index = 0;
+
+                _frame.fb = esp_camera_fb_get();
+                if (_frame.fb == NULL) {
+                    log_e("Camera frame failed");
+                    return 0;
+                }
+
+                if(_frame.fb->format != PIXFORMAT_JPEG){
+                    unsigned long st = millis();
+                    bool jpeg_converted = frame2jpg(_frame.fb, 80, &_jpg_buf, &_jpg_buf_len);
+                    if(!jpeg_converted){
+                        log_e("JPEG compression failed");
+                        esp_camera_fb_return(_frame.fb);
+                        _frame.fb = NULL;
+                        _jpg_buf_len = 0;
+                        _jpg_buf = NULL;
+                        return 0;
+                    }
+                    log_i("JPEG: %lums, %uB", millis() - st, _jpg_buf_len);
+                } else {
+                    _jpg_buf_len = _frame.fb->len;
+                    _jpg_buf = _frame.fb->buf;
+                }
+
+                //send boundary
+                size_t blen = 0;
+                if(index){
+                    blen = strlen(STREAM_BOUNDARY);
+                    memcpy(buffer, STREAM_BOUNDARY, blen);
+                    buffer += blen;
+                }
+                //send header
+                size_t hlen = sprintf((char *)buffer, STREAM_PART, JPG_CONTENT_TYPE, _jpg_buf_len);
+                buffer += hlen;
+                //send frame
+                hlen = maxLen - hlen - blen;
+                if(hlen > _jpg_buf_len){
+                    maxLen -= hlen - _jpg_buf_len;
+                    hlen = _jpg_buf_len;
+                }
+                memcpy(buffer, _jpg_buf, hlen);
+                _frame.index += hlen;
+                return maxLen;
+            }
+
+            size_t available = _jpg_buf_len - _frame.index;
+            if(maxLen > available){
+                maxLen = available;
+            }
+            memcpy(buffer, _jpg_buf+_frame.index, maxLen);
+            _frame.index += maxLen;
+
+            return maxLen;
+        }
+};
--- ESP32-CAM/video-streaming/src/main.cpp
+++ ESP32-CAM/video-streaming/src/main.cpp
@@ -1,18 +1,66 @@
 #include <Arduino.h>
+// esp32cam with async web server
+#include <AsyncWebCamera.h>
+// wifi credentials (create your own file on include/Secrets.h)
+#include <Secrets.h>
+// wifi library
+#include <WiFi.h>
+// upload files to file system
+#include <FS.h>
+#include <LittleFS.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
+//Web server credentials
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+AsyncWebServer server(80);
+
+
+/*FUNCTION DECLARATION------------------------------------------------------------------------------------------------------*/
+void streamJpg(AsyncWebServerRequest *request);
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+/*VOID SETUP CONFIGURATION--------------------------------------------------------------------------------------------------*/
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(115200);
+  // initialize camera (ESP32-CAM AI-Thinker configuration)
+  Camera_init_cofig();
+  // web server as Station mode:
+  // we can  request information from the internet 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  else{
+    Serial.printf("WiFi connected succesfully!");
+  }
+  // shows web page IP
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // stream video
+  server.on("/stream", HTTP_GET, streamJpg);
+  server.begin();
 
-void loop() {
-  // put your main code here, to run repeatedly:
 }
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/*LOOP----------------------------------------------------------------------------------------------------------------------*/
+void loop(){
+
 }
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+/*FUNCTIONS-----------------------------------------------------------------------------------------------------------------*/
+void streamJpg(AsyncWebServerRequest *request){
+    AsyncJpegStreamResponse *response = new AsyncJpegStreamResponse();
+    if(!response){
+        request->send(501);
+        return;
+    }
+    response->addHeader("Access-Control-Allow-Origin", "*");
+    request->send(response);
+}
+/*--------------------------------------------------------------------------------------------------------------------------*/
